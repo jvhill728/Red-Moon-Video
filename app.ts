fa45306a76e46f1bd6d5626941a0df48db9169e0
jvhill728 <jvhill728@gmail.com>
2022-06-21 18:13:13 -0500

    setting up middleware, exporting server

--- app.ts
+++ app.ts
@@ -1,6 +1,7 @@
 import express, { Application, Request, Response, NextFunction } from 'express';
 require('dotenv').config();
 const server: Application = express();
+const PORT = process.env.PORT || 4000;
 
 // Using req, res, next to show something on localhost:4000
 server.get('/', (req: Request, res: Response) => {
@@ -13,5 +14,8 @@ server.listen(4000, () => console.log('Server is up'));
 const cors = require('cors');
 server.use(cors());
 
+const morgan = require('morgan');
+server.use(express.json());
 
 
+module.exports = server;
