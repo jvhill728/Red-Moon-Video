c8a43ebd248a111e196790a3315d32e4c79dc7ca
jvhill728 <jvhill728@gmail.com>
2022-07-03 14:04:00 -0500

    createUser function created

--- db/models/users.js
+++ db/models/users.js
@@ -1,4 +1,28 @@
 const client = require('../client');
 const bcrypt = require('bcrypt');
 
-const createUser = async ({username, password, isAdmin})
+const createUser = async ({username, password, email}) => {
+    try {
+        console.log('Creating Users');
+
+        const SALT_COUNT = 10;
+        const hashedPassword = await bcrypt.hash(password, SALT_COUNT);
+
+        const { rows: [users] } = await client.query(`
+        INSERT INTO users (username, password, email)
+        VALUES ($1, $2, $3)
+        ON CONFLICT (username) DO NOTHING
+        RETURNING username, id, email;
+        `, [username, hashedPassowrd, email])
+
+        return users;
+    } catch (error) {
+        throw error
+    }
+}
+
+
+
+module.exports = {
+    createUser,
+}
