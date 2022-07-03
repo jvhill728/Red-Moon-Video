44096aac8a3579f17bffd53cab100a2c2d003d84
jvhill728 <jvhill728@gmail.com>
2022-07-03 15:03:12 -0500

    getUser function created

--- db/models/users.js
+++ db/models/users.js
@@ -45,7 +45,27 @@ const getAllUsers = async () => {
 }
 
 const getUser = async (username, password, email) => {
-    
+    console.log('username ===>', username)
+    console.log('password ===>', password)
+
+    try {
+        const user = await getUserByUsername(username);
+        const hashedPassword = user.password;
+
+        const verifyPassword = await bcrypt.compare(password, hashedPassword);
+        console.log('PW', password)
+        console.log('HPW', hashedPassword)
+        console.log('VPW', verifyPassword)
+
+        if(verifyPassword) {
+            delete user.password;
+            console.log('User after deleted password', user)
+
+            return user;
+        }
+    } catch (error) {
+        throw error
+    }
 }
 
 
@@ -53,4 +73,5 @@ const getUser = async (username, password, email) => {
 module.exports = {
     createUser,
     getAllUsers,
+    getUser,
 }
