58158a676bc45dedbdfc19286829cd0bf865da9c
jvhill728 <jvhill728@gmail.com>
2022-07-09 11:17:30 -0500

    movies functions created in models

--- db/models/movies.js
+++ db/models/movies.js
@@ -1,2 +1,54 @@
 const client = require('../client');
 
+const createMovie = async ({
+    title, releaseDate, genre, tags
+}) => {
+    try {
+        const { rows: [movie] } = await client.query(`
+        INSERT INTO movies (title,
+            "releaseDate",
+            genre,
+            tags)
+        VALUES($1, $2, $3, $4)
+        RETURNING *;
+        `, [title, releaseDate, genre, tags])
+
+        return movie
+    } catch (error) {
+        throw error
+    }
+}
+
+const getAllMovies = async () => {
+    try {
+        const { rows: movies } = await client.query(`
+        SELECT * 
+        FROM movies
+        `)
+
+        console.log('These are our movies', movies)
+
+        return movies
+    } catch (error) {
+        throw error
+    }
+}
+
+const getMovieById = async (id) => {
+    try {
+        const { rows: [movie] } = await client.query(`
+        SELECT * FROM movies
+        WHERE id = ($1);
+        `, [id])
+
+        return movie
+    } catch (error) {
+        throw error
+    }
+}
+
+module.exports = {
+    createMovie,
+    getAllMovies,
+    getMovieById
+}
