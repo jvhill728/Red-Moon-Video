c9f8a7e3bf2d075997be7c78902b424c152f67a9
jvhill728 <jvhill728@gmail.com>
2022-08-01 19:38:48 -0500

    navBar is 90% complete

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,16 +9,24 @@ const NavBar = (props) => {
             <div id="RMNavBar" className="navText">
                 Red Moon Video
             </div>
+            <span className="navText">
+                <button>
+                    <Link to="/"> Home </Link>
+                </button>
+                <button>
+                    <Link to="/register"> Register </Link>
+                </button>
+                <button>
+                    <Link to="/login"> Login </Link>
+                </button>
+                <button>
+                    <Link to="/videoclerk"> Video Clerk </Link>
+                </button>
+                <button>
+                    <Link to="/library"> Video Library </Link>
+                </button>
+            </span>
 
-            <div id="siteLinks">
-                {isLoggedIn ?
-                    <span className="welcomeUser">{`Welcome, ${loggedInUsername}`}</span>
-                    :
-                    <span className="welcomeUser">Welcome Dear Guest!</span>
-                }
-
-                <Link to="/" className="navText">Home</Link>    
-            </div>
         </div>
     )
 }
