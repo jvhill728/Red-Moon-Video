0a910c694623f9a2884a260ba546bc75d28ad9b0
jvhill728 <jvhill728@gmail.com>
2022-08-08 18:00:44 -0500

    registernewUser created

--- src/api/index.js
+++ src/api/index.js
@@ -16,4 +16,29 @@ export const getAllUsers = async () => {
     }
 }
 
-const 
+export const registerNewUser = async (userObject) => {
+
+    console.log("UserObject", userObject)
+    const response = await fetch(`${baseUrl}/users/register`. {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            username: userObject.username,
+            password: userObject.password
+        }),
+    });
+    const json = await response.json()
+    const user = json.user
+
+    console.log('JSON', json)
+    console.log('USER==>', user)
+    console.log('userObject', userObject)
+
+    localStorage.setItem('UserToken', json.token);
+    localStorage.setItem('Username', user.username);
+    localStorage.setItem('userId', user.id);
+
+    return json;
+}
